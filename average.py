ce1991a3cc4e963786b4e270ef38786055089b7e
WildRelation <joseph.solsol@hotmail.com>
2023-01-01 22:13:51 +0100

    average.py

---
+++ average.py
@@ -0,0 +1,8 @@
+def main():
+    print ("This program computes the average of two exam scores. ")
+
+    score1, score2 = eval(input("Enter the scores separated by a coma: "))
+    average = (score1 + score2)/2
+    print("The average is", average)
+
+main()
